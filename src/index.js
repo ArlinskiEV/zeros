390e092932160acadfb6689997c60a23ed7fc65d
ArlinskiEV <ArlinskiEV@gmail.com>
2018-02-26 21:31:45 +0300

    zeros/fix: remove unusful variable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
   let five = 0;
-  let pow = 0;
   let temp = 1;
 
   while (temp * 5 <= number) {
     temp *= 5;
-    pow += 1;
     five += Math.trunc(number / temp);
   }
 
