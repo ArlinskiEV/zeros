5f49cdfc26985940fb6f67991852583734189481
ArlinskiEV <ArlinskiEV@gmail.com>
2018-02-26 21:25:46 +0300

    zeros/features: one loop

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
   let five = 0;
-  let pow = 1;
-  let temp = 5;
+  let pow = 0;
+  let temp = 1;
 
   while (temp * 5 <= number) {
     temp *= 5;
     pow += 1;
-  }
-
-  while (temp > 1) {
     five += Math.trunc(number / temp);
-    temp /= 5;
-    pow -= 1;
   }
 
   return five;
