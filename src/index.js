7a1d65a0b139190e80fc5e6b99b5e61d0404b45a
ArlinskiEV <ArlinskiEV@gmail.com>
2018-02-26 21:23:35 +0300

    zros/features: pow

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 module.exports = function getZerosCount(number) {
   // your implementation
   let five = 0;
-  while (number % 5 !== 0) {
-    number -= 1;
+  let pow = 1;
+  let temp = 5;
+
+  while (temp * 5 <= number) {
+    temp *= 5;
+    pow += 1;
   }
 
-  // let pow = 1;
-  // let temp = 5;
-  // while (temp * 5 <= number) {
-  //   temp *= 5;
-  // }
-
-  while (number > 0) {
-    let i = number;
-    while (i % 5 === 0) {
-      five += 1;
-      i /= 5;
-    }
-    number -= 5;
+  while (temp > 1) {
+    five += Math.trunc(number / temp);
+    temp /= 5;
+    pow -= 1;
   }
-
-  // temp = 5^pow = current number
-  // while (number > 0) {
-  //   five += pow;
-  //   temp /= 5;
-  //   pow -= 1;
-
-  // }
 
   return five;
 }
